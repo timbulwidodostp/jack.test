72e14edc104628d854e1015ee1f6d40fa1985295
Timbul Widodo, S.TP <160803827+timbulwidodostp@users.noreply.github.com>
2025-05-02 06:21:41 +0700

    Use jack.test (pls) With R Software

---
+++ jack.test/jack.test.R
@@ -0,0 +1,17 @@
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# Jackknife approximate t tests of regression coefficients Use jack.test (pls) With R Software
+install.packages("pls")
+library("pls")
+jack.test = read.csv("https://raw.githubusercontent.com/timbulwidodostp/jack.test/main//jack.test.csv",sep = ";")
+# Estimation Jackknife approximate t tests of regression coefficients Use jack.test (pls) With R Software
+chemical <- cbind(jack.test$chemical.Acidity, jack.test$chemical.Peroxide, jack.test$chemical.K232, jack.test$chemical.K270, jack.test$chemical.DK)
+sensory <- cbind(jack.test$sensory.yellow, jack.test$sensory.green,jack.test$sensory.brown, jack.test$sensory.glossy, jack.test$sensory.transp, jack.test$sensory.syrup)
+jack.test <- pcr(sensory ~ chemical, data = jack.test, validation = "LOO", jackknife = TRUE)
+jack.test(jack.test, ncomp = 2)
+# Jackknife approximate t tests of regression coefficients Use jack.test (pls) With R Software
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# Finished
