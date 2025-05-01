2f1249d97c58dfeb5c1b22734ce94f8b32221846
Timbul Widodo, S.TP <160803827+timbulwidodostp@users.noreply.github.com>
2025-05-02 06:34:45 +0700

    Use jack.test (pls) With R Software

--- jack.test/jack.test.R
+++ jack.test/jack.test.R
@@ -4,7 +4,7 @@
 # Jackknife approximate t tests of regression coefficients Use jack.test (pls) With R Software
 install.packages("pls")
 library("pls")
-jack.test = read.csv("https://raw.githubusercontent.com/timbulwidodostp/jack.test/main//jack.test.csv",sep = ";")
+jack.test = read.csv("https://raw.githubusercontent.com/timbulwidodostp/jack.test/main/jack.test/jack.test.csv",sep = ";")
 # Estimation Jackknife approximate t tests of regression coefficients Use jack.test (pls) With R Software
 chemical <- cbind(jack.test$chemical.Acidity, jack.test$chemical.Peroxide, jack.test$chemical.K232, jack.test$chemical.K270, jack.test$chemical.DK)
 sensory <- cbind(jack.test$sensory.yellow, jack.test$sensory.green,jack.test$sensory.brown, jack.test$sensory.glossy, jack.test$sensory.transp, jack.test$sensory.syrup)
